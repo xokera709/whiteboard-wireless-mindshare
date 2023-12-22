2a4cfec79929ff41bfdfa25c5836dca65b21c877
xokera709 <148460685+xokera709@users.noreply.github.com>
2023-12-22 17:35:05 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+
+        for (int item : data) {
+public class RandomDataGenerator {
+        }
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+
+        }
+}
+        for (int i = 0; i < 10; i++) {
+    }
